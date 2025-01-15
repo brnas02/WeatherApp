56b9074b9a303f0cd372518a767c8444aba77232
brnas02 <aluno23025@ipt.pt>
2025-01-15 18:22:51 +0100

    Tomorrow Activity UI Design

---
+++ app/src/main/java/com/example/weatherapp/Activities/TomorrowActivity.java
@@ -0,0 +1,14 @@
+package com.example.weatherapp.Activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.weatherapp.R;
+
+public class TomorrowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tomorrow);
+    }
+}
---
+++ app/src/main/java/com/example/weatherapp/Adapters/TomorrowAdapter.java
@@ -0,0 +1,71 @@
+package com.example.weatherapp.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.weatherapp.Domains.Hourly;
+import com.example.weatherapp.Domains.TomorrowDomain;
+import com.example.weatherapp.R;
+
+import java.util.ArrayList;
+
+public class TomorrowAdapter extends RecyclerView.Adapter<TomorrowAdapter.ViewHolder> {
+    ArrayList<TomorrowDomain> items;
+    Context context;
+
+    public TomorrowAdapter(ArrayList<TomorrowDomain> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public TomorrowAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_tomorrow, parent, false);
+        context = parent.getContext();
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TomorrowAdapter.ViewHolder holder, int position) {
+
+        holder.dayTxt.setText(items.get(position).getDay());
+        holder.statusTxt.setText(items.get(position).getStatus());
+        holder.highTxt.setText(items.get(position).getHighTemp());
+        holder.lowTxt.setText(items.get(position).getLowTemp());
+
+        int drawableResourceId=holder.itemView.getResources()
+                .getIdentifier(items.get(position).getPicPath(), "drawable",holder.itemView.getContext().getPackageName());
+
+        Glide.with(context)
+                .load(drawableResourceId)
+                .into(holder.pic);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView dayTxt, statusTxt, lowTxt, highTxt;
+        ImageView pic;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            dayTxt = itemView.findViewById(R.id.dayTxt);
+            statusTxt = itemView.findViewById(R.id.statusTxt);
+            lowTxt = itemView.findViewById(R.id.lowTxt);
+            highTxt = itemView.findViewById(R.id.highTxt);
+            pic=itemView.findViewById(R.id.pic);
+        }
+    }
+}
